45e5da6ec59608026ddf4422e6f8a03be5b415b8
Fotis Fotiadis <skioulis@gmail.com>
2024-04-09 11:50:27 +0300

    Bing Autosuggest

---
+++ BlazorApp/Client/Pages/Bing.razor.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+using BlazorApp.Shared.Keys;
+using BlazorApp.Shared.Domain;
+
+namespace BlazorApp.Client.Pages
+{
+    public partial class Bing
+    {
+        [Inject] HttpClient _client { get; set; }
+        public string UserInput { get; set; }
+        public string BingMapsKey { get; set; }
+        BingResponseModel PlacesFound { get; set; }
+        string errorString;
+
+
+        protected async override Task OnInitializedAsync()
+        {
+            BingMapsKey = ApiKey.BingKey;
+
+        }
+
+
+
+        private async Task HandleSubmit2()
+        {
+            var url = $"https://dev.virtualearth.net/REST/v1/Autosuggest?key={BingMapsKey}&query={UserInput}";
+
+
+            try
+            {
+                PlacesFound = await _client.GetFromJsonAsync<BingResponseModel>(url);
+                errorString = null;
+            }
+            catch (Exception ex)
+            {
+
+                errorString = errorString = $"Error Getting the data {ex.Message}";
+            }
+        }
+
+    }
+}
---
+++ BlazorApp/Shared/Domain/BingResponseModel.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp.Shared.Domain
+{
+    public class BingResponseModel
+    {
+        public string AuthenticationResultCode { get; set; }
+
+        public Resourceset[] ResourceSets { get; set; }
+        public int StatusCode { get; set; }
+        public string StatusDescription { get; set; }
+
+    }
+
+    public class Resourceset
+    {
+
+        public Resource[] Resources { get; set; }
+    }
+
+    public class Resource
+    {
+        public string Type { get; set; }
+        public Value[] value { get; set; } = null!;
+    }
+
+    public class Value
+    {
+        public string Type { get; set; }
+        public Address Address { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class Address
+    {
+        public string CountryRegion { get; set; }
+        public string Locality { get; set; }
+        public string AdminDistrict { get; set; }
+        public string AdminDistrict2 { get; set; }
+        public string CountryRegionIso2 { get; set; }
+        public string FormattedAddress { get; set; }
+    }
+}
+
